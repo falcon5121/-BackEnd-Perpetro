617fd170a1d795100f31e1679b0d86124da12b62
Emanuel da Silva Gonçalves <fxemanuel@gmail.com>
2023-09-13 21:27:57 -0300

    tentando de novo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app
   )
   .use(
     cors({
-      origin: "https://main--vermillion-monstera-6ce323.netlify.app/",
+      origin: "*",
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       preflightContinue: false,
       credentials: true,
